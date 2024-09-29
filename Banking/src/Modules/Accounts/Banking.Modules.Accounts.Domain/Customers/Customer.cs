f056a7424f901d3fb83339c5d4248aa191620189
Deyverson Batista <deyverson@outlook.com>
2024-09-29 16:05:06 +0200

    fix test project failed and adding in actions verify when pullrequest some file

--- Banking/src/Modules/Accounts/Banking.Modules.Accounts.Domain/Customers/Customer.cs
+++ Banking/src/Modules/Accounts/Banking.Modules.Accounts.Domain/Customers/Customer.cs
@@ -3,6 +3,7 @@
 namespace Banking.Modules.Accounts.Domain.Customers;
 public sealed class Customer : Entity
 {
+    private Customer() { }
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Surname { get; private set; }
