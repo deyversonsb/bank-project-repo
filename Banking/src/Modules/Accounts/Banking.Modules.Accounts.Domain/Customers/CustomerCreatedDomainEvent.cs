c96a013e2b18f6dcee7760256c680c0e3302df32
Deyverson Batista <deyverson@outlook.com>
2024-09-29 22:01:49 +0200

    fix test project failed

--- Banking/src/Modules/Accounts/Banking.Modules.Accounts.Domain/Customers/CustomerCreatedDomainEvent.cs
+++ Banking/src/Modules/Accounts/Banking.Modules.Accounts.Domain/Customers/CustomerCreatedDomainEvent.cs
@@ -1,7 +1,7 @@
 ﻿using Banking.Common.Domain;
 
 namespace Banking.Modules.Accounts.Domain.Customers;
-public class CustomerCreatedDomainEvent(Guid customerId, decimal initialCredit) : DomainEvent
+public sealed class CustomerCreatedDomainEvent(Guid customerId, decimal initialCredit) : DomainEvent
 {
     public Guid CustomerId { get; init; } = customerId;
     public decimal InitialCredit { get; init; } = initialCredit;
